3021479948af5973450f2589419f251a58c8abb4
Ufuk Uzun <ufukuzun.ce@gmail.com>
2015-01-16 23:57:36 +0200

    deprecated codes and not used methods cleaned

--- myth-dialect/src/main/java/com/ufukuzun/myth/dialect/util/ElementAndAttrUtils.java
+++ myth-dialect/src/main/java/com/ufukuzun/myth/dialect/util/ElementAndAttrUtils.java
@@ -16,85 +16,57 @@
 
 package com.ufukuzun.myth.dialect.util;
 
-import java.util.List;
-
-import org.thymeleaf.Arguments;
-import org.thymeleaf.dom.Document;
-import org.thymeleaf.dom.Element;
-import org.thymeleaf.standard.expression.StandardExpressionProcessor;
-
 import com.ufukuzun.myth.dialect.MythDialect;
+import org.thymeleaf.Arguments;
+import org.thymeleaf.dom.Element;
+import org.thymeleaf.standard.expression.IStandardExpression;
+import org.thymeleaf.standard.expression.IStandardExpressionParser;
+import org.thymeleaf.standard.expression.StandardExpressions;
+
+import java.util.List;
 
 public class ElementAndAttrUtils {
 
-	public static Element getBodyElement(Document document) {
-		return getFirstLevelElement(document, "body");
-	}
-	
-	public static Element getHeadElement(Document document) {
-		return getFirstLevelElement(document, "head");
-	}
-	
-	private static Element getFirstLevelElement(Document document, String elementName) {
-		for (Element each : getElementByName(document.getElementChildren(), "html").getElementChildren()) {
-			if (each.getUnprefixedNormalizedName().equalsIgnoreCase(elementName)) {
-				return each;
-			}
-		}
-		
-		return null;
-	}
-	
-	public static Element getElementByName(List<Element> elements, String elementName) {
-		for (Element each : elements) {
-			if (each.getNormalizedName().equalsIgnoreCase(elementName)) {
-				return each;
-			} else if (hasElementChildren(each)) {
-				return getElementByName(each.getElementChildren(), elementName);
-			}
-		}
-		
-		return null;
-	}
-	
-	public static Element getElementById(List<Element> elements, String elementId) {
-		for (Element each : elements) {
-			if (equalsAttribute(each, "id", elementId)) {
-				return each;
-			} else if (hasElementChildren(each)) {
-				return getElementById(each.getElementChildren(), elementId);
-			}
-		}
-		
-		return null;
-	}
-	
-	public static boolean equalsAttribute(Element element, String attribute, String value) {
-		return element.hasAttribute(attribute) && element.getAttributeValue(attribute).equals(value);
-	}
-	
-	public static String getPrefixedName(String elementName) {
-		return MythDialect.DIALECT_PREFIX + ":" + elementName;
-	}
-	
-	public static boolean hasElementChildren(Element element) {
-		return !element.getElementChildren().isEmpty();
-	}
-	
-	public static String getProcessedAttributeValue(final Arguments arguments, final Element element, final String attributeName) {
-		String attributeValue = element.getAttributeValue(attributeName);
-		return getProcessedAttributeValue(arguments, attributeValue);
-	}
-	
-	public static String getProcessedAttributeValue(final Arguments arguments, final String attributeValue) {
-		Object result = null;
-		try {
-			result = StandardExpressionProcessor.processExpression(arguments, attributeValue);
-		} catch (Exception e) {
-			// TODO: log the exception with Spring's logger
-		}
+    public static Element getElementById(List<Element> elements, String elementId) {
+        for (Element each : elements) {
+            if (equalsAttribute(each, "id", elementId)) {
+                return each;
+            } else if (hasElementChildren(each)) {
+                return getElementById(each.getElementChildren(), elementId);
+            }
+        }
 
-		return (result == null ? attributeValue : result.toString());
-	}
-	
+        return null;
+    }
+
+    public static boolean equalsAttribute(Element element, String attribute, String value) {
+        return element.hasAttribute(attribute) && element.getAttributeValue(attribute).equals(value);
+    }
+
+    public static String getPrefixedName(String elementName) {
+        return MythDialect.DIALECT_PREFIX + ":" + elementName;
+    }
+
+    public static boolean hasElementChildren(Element element) {
+        return !element.getElementChildren().isEmpty();
+    }
+
+    public static String getProcessedAttributeValue(final Arguments arguments, final Element element, final String attributeName) {
+        String attributeValue = element.getAttributeValue(attributeName);
+        return getProcessedAttributeValue(arguments, attributeValue);
+    }
+
+    public static String getProcessedAttributeValue(final Arguments arguments, final String attributeValue) {
+        Object result = null;
+
+        try {
+            IStandardExpressionParser expressionParser = StandardExpressions.getExpressionParser(arguments.getConfiguration());
+            IStandardExpression expression = expressionParser.parseExpression(arguments.getConfiguration(), arguments, attributeValue);
+            result = expression.execute(arguments.getConfiguration(), arguments);
+        } catch (Exception e) {
+        }
+
+        return result == null ? attributeValue : result.toString();
+    }
+
 }
